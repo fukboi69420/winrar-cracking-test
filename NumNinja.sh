b3057e9c149514d9f3cdfa8a54ca73ef8d590df0
fukboi69420 <82789164+fukboi69420@users.noreply.github.com>
2023-01-26 21:22:34 -0800

    Create NumNinja.sh

---
+++ NumNinja.sh
@@ -0,0 +1,10 @@
+git clone https://github.com/SHUR1K-N/NumNinja-Numeric-Wordlist-Generator
+cd NumNinja-Numeric-Wordlist-Generator
+pip install -r requirements.txt
+pip install tqdm #(for progress bars)
+pip install tkinter #(for GUI elements)
+pip install colorama #(for colors)
+pip install termcolor #(for colors)
+cd ..
+cd ..
+ls
