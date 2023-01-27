c1dd2fe62c6c5693e0a9eb0a2d814b51f347e988
fukboi69420 <82789164+fukboi69420@users.noreply.github.com>
2023-01-26 21:18:19 -0800

    Create rarninja.sh

---
+++ rarninja.sh
@@ -0,0 +1,11 @@
+git clone https://github.com/SHUR1K-N/RARNinja-RAR-Password-Cracking-Utility
+cd RARNinja
+#install requirements
+pip install -r requirements.txt
+#install dependencies
+pip install rarfile -y
+pip install colorama -y
+pip install termcolor -y
+ls
+#launch program
+python3 RARNinja.py
